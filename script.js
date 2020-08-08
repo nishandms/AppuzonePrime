58184c698c15dd050501ca522455500bb277773d
nishandms <66947607+nishandms@users.noreply.github.com>
2020-08-08 20:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function myFunction() {
   var x = document.getElementById("id1").value;
   var y = "14321";
   if (x==y) {
-    window.open("list.html")
+    window.open("./list.html")
   } else {
     alert("Poi ticket edukkeda naari")
   } 
